5fc5c2ba9efdf9e12ecb1c9a29290c44a20ffb45
Alexis Lopez Zubieta <azubieta90@gmail.com>
2019-04-17 17:04:35 -0500

    Refator project structure

--- build-conan-appimage.sh
+++ build-conan-appimage.sh
@@ -1,1 +1,24 @@
 #!/usr/bin/env bash
+
+set -xe
+
+SOURCES_DIR=${PWD}
+
+rm build -rf
+mkdir build && cd build
+
+wget -nc https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage
+wget -nc https://raw.githubusercontent.com/linuxdeploy/linuxdeploy-plugin-conda/master/linuxdeploy-plugin-conda.sh
+
+chmod +x linuxdeploy-*
+
+rm AppDir -rf
+
+mkdir -p AppDir/usr/share/applications
+cp ${SOURCES_DIR}/res/io.conan.desktop AppDir/usr/share/applications/
+
+mkdir -p AppDir/usr/share/icons/hicolor/192x192/apps
+cp ${SOURCES_DIR}/res/conan.png AppDir/usr/share/icons/hicolor/192x192/apps/
+
+export CONDA_PACKAGES=conan
+./linuxdeploy-x86_64.AppImage --appdir=AppDir --plugin conda --output appimage
