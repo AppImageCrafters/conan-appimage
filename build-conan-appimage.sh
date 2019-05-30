7b1d5b805dae8a360688406efd1797b4341d4492
Alexis Lopez Zubieta <contact@azubieta.net>
2019-05-30 10:47:04 -0500

    Improve build script

--- build-conan-appimage.sh
+++ build-conan-appimage.sh
@@ -4,13 +4,17 @@ set -xe
 
 SOURCES_DIR=${PWD}
 
-rm build -rf
-mkdir build && cd build
+rm build/AppDir -rf
+mkdir -p build
+pushd build
 
-wget -nc https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage
-wget -nc https://raw.githubusercontent.com/linuxdeploy/linuxdeploy-plugin-conda/master/linuxdeploy-plugin-conda.sh
-
+wget -qnc https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage
 chmod +x linuxdeploy-*
+./linuxdeploy-x86_64.AppImage --appimage-extract
+
+wget -qnc https://raw.githubusercontent.com/linuxdeploy/linuxdeploy-plugin-conda/master/linuxdeploy-plugin-conda.sh \
+    -O squashfs-root/usr/bin/linuxdeploy-plugin-conda.sh
+chmod +x squashfs-root/usr/bin/linuxdeploy-plugin-conda.sh
 
 rm AppDir -rf
 
@@ -23,4 +27,10 @@ cp ${SOURCES_DIR}/res/conan.png AppDir/usr/share/icons/hicolor/192x192/apps/
 cp ${SOURCES_DIR}/AppRun AppDir
 
 export CONDA_PACKAGES=conan
-./linuxdeploy-x86_64.AppImage --appdir=AppDir --plugin conda --output appimage
+export UPDATE_INFORMATION="gh-releases-zsync|azubieta|conan-appimage|continuous|conan*$ARCH*.AppImage.zsync"
+export SIGN=1
+export VERBOSE=1
+
+squashfs-root/AppRun --appdir=AppDir --plugin conda
+squashfs-root/AppRun --appdir=AppDir --output appimage
+popd
